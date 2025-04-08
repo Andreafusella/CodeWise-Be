1b56319507666a29dff8a88a3b99b2a8faf7d25b
Andreafusella <andreafusellabusiness@gmail.com>
2025-04-08 15:32:39 +0200

    first commit

---
+++ src/test/java/com/codeWise/codeWise/CodeWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeWise.codeWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
